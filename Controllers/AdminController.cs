65c9892f472dd2d6ace1656e8c49db89dc5a8522
Ismael <villavicencioismael@gmail.com>
2022-02-09 09:55:19 -0600

    #initial commit

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prueba.Controllers
+{
+    [Route("/manage")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
